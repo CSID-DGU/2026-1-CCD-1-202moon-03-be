8f04c02425fec8152ed3472165e0014786a40ec2
lsoobin <vini20012203@gmail.com>
2026-06-08 17:00:58 +0900

    chore: admin.py 수정

--- analytics/admin.py
+++ analytics/admin.py
@@ -4,5 +4,18 @@ from .models import LearningResult
 
 @admin.register(LearningResult)
 class LearningResultAdmin(admin.ModelAdmin):
-    list_display = ["id", "session", "watch_rate", "total_score", "max_combo", "typing_accuracy", "quiz_correct", "quiz_total", "completed_at"]
+    list_display = ["id", "get_username", "get_title", "get_mode", "watch_rate", "total_score", "max_combo", "typing_accuracy", "quiz_correct", "quiz_total", "tab_switch_count", "study_duration_seconds", "completed_at"]
+    list_filter = ["session__mode", "completed_at"]
     search_fields = ["session__title", "session__user__username"]
+
+    def get_username(self, obj):
+        return obj.session.user.username
+    get_username.short_description = "유저"
+
+    def get_title(self, obj):
+        return obj.session.title
+    get_title.short_description = "영상 제목"
+
+    def get_mode(self, obj):
+        return obj.session.mode
+    get_mode.short_description = "모드"
